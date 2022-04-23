15dd83df7d6d580f6a8a387cae9a5e912872fead
kripa0047 <kripabaiagi0047@gmail.com>
2022-04-23 20:18:20 +0530

    err log

--- src/v1/Memer/controller/memerController.js
+++ src/v1/Memer/controller/memerController.js
@@ -222,6 +222,7 @@ exports.addMemes = async (req, res, next) => {
     };
     return res.status(200).json(returnData);
   } catch (error) {
+    console.log(error);
     return res.status(500).json(error);
   }
 };

c859072285e209f88df2f39134551f8711ee9766
kripa0047 <kripabaiagi0047@gmail.com>
2022-05-11 08:21:49 +0530

    default pic added

--- src/NOSQL/models/memer.js
+++ src/NOSQL/models/memer.js
@@ -7,7 +7,7 @@ const MemerSchema = mongoose.Schema(
     dob: { type: Number, required: true },
     email: { type: String, required: true },
     phoneNo: { type: Number, default: null },
-    picture: { type: String, default: "" },
+    picture: { type: String, default: "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg" },
     price: { type: Number, required: true },
     fcmToken: { type: String, default: null },
     isDeleted: { type: Boolean, default: false },
